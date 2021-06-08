e898453b15cd10c546a3f50dacf5b2953aa57907
gordan <gordan790530@gmail.com>
2021-06-08 15:55:05 +0800

    1st version

---
+++ Gitbranchsample/src/GitbranchApp/MainApp.java
@@ -0,0 +1,11 @@
+package GitbranchApp;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      //這是測試第一版
+		System.out.println("1st version");
+	}
+
+}
